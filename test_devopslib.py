df4ecebdd70ccd82c00c0d005898397d60d30b19
root <root@kverma-home>
2023-08-15 16:02:40 +0530

    modified test

--- test_devopslib.py
+++ test_devopslib.py
@@ -3,4 +3,4 @@ from devopslib.randomfruit import fruit
 
 def test_fruit():
     fruit_choice = fruit()
-    assert fruit_choice in ["apple", "banana", "orange"]
+    assert fruit_choice in ["apple", "cherry", "strawberry"]
